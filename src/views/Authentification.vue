0040c85203c66a5fa0b874f4d7d9a86a05633530
William <william.broways@gmail.com>
2019-10-16 14:54:03 +0200

    test

--- src/views/Authentification.vue
+++ src/views/Authentification.vue
@@ -72,7 +72,15 @@ export default {
     },
     reset() {
       this.$refs.form.reset();
+    },
+    sleep(milliseconds) {
+      var start = new Date().getTime();
+      for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
     }
+  }
   },
   mounted() {
     if (localStorage.token) {
