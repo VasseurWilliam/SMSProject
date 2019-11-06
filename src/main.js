334ab75ab3433ca0d6c79f4c5b3fc442e99bf71c
William <william.broways@gmail.com>
2019-11-06 16:30:33 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store/store";
 import vuetify from "./plugins/vuetify";
 import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
 import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
