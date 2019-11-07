0e5dff7068696b97cf0a3115638a233d7b01a757
William <william.broways@gmail.com>
2019-11-07 14:03:27 +0100

    test

--- src/views/Details_facture_client.vue
+++ src/views/Details_facture_client.vue
@@ -1,10 +1,10 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="desserts"
+    :items="coach"
     :single-expand="singleExpand"
     :expanded.sync="expanded"
-    item-key="name"
+    item-key="nom"
     show-expand
     class="elevation-1"
   >
@@ -21,103 +21,33 @@ import axios from "axios";
     data () {
       return {
         expanded: [],
+        coach:[],
         singleExpand: true,
         headers: [
           {
-            text: 'Dessert (100g serving)',
+            text: 'Nom',
             align: 'left',
             sortable: false,
-            value: 'name',
+            value: 'nom',
           },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
+          { text: 'Prenom', value: 'prenom' },
+          { text: 'Facture total', value: 'facture_mois' },
           { text: '', value: 'data-table-expand' },
         ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
+      }
+    },
+    methods: {
+
+    },
+    async mounted() {
+      const response = await axios.get("https://sportmanagementsystemapi.herokuapp.com/api/client");
+      for (var x = 0; x < response.data.data.length; x++) {
+        this.coach.push({
+          nom: response.data.data[x].nom,
+          prenom: response.data.data[x].prenom,
+          facture_mois: response.data.data[x].facture_mois,
+        });
+        console.log(this.coach);
       }
     },
   }
