4ae12f732e74b59142032a6e3a4f82177aa5d5ba
william <vasseur.william1234@outlook.fr>
2020-01-29 16:26:32 +0100

    make it better

---
+++ src/components/ModifCoach.vue
@@ -0,0 +1,85 @@
+<template>
+    <div align="center">
+        <v-dialog v-model="dialog" persistent max-width="800px">
+            <template v-slot:activator="{ on }">
+                <v-btn class="v-btn success" v-on="on">Modifier</v-btn>
+            </template>
+            <v-card>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="Prénom"
+                                        required
+                                        v-model="prenom"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="Nom"
+                                        required
+                                        v-model="nom"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="Email"
+                                        required
+                                        v-model="email"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="Spécialité"
+                                        required
+                                        v-model="specialite"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="couleur"
+                                        required
+                                        v-model="couleur"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="Facture Mois"
+                                        required
+                                        v-model="facture_mois"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                        label="Facture Coach"
+                                        required
+                                        v-model="facture_coach"
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="red darken-1" text @click="dialog = false"
+                    >Fermer</v-btn>
+                    <v-btn color="blue darken-1" text @click="submitmodif"
+                    >Valider</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        data() {
+            return {
+                dialog: false
+            };
+        },
+        props: ['prenom', 'nom', 'email', 'specialite', 'couleur', 'facture_mois', 'facture_coach', 'submitmodif']
+    }
+</script>
---
+++ src/views/Coach.vue
@@ -0,0 +1,123 @@
+<template>
+    <v-container fluid>
+        <v-data-iterator
+                :items="coach"
+                :items-per-page.sync="itemsPerPage"
+                hide-default-footer
+        >
+
+            <template v-slot:default="props">
+                <v-row>
+                    <v-col
+                            v-for="item in coach"
+                            :key="item.name"
+                            cols="12"
+                            sm="6"
+                            md="4"
+                            lg="3"
+                    >
+                        <v-card>
+                            <v-card-title class="subheading font-weight-bold">{{ item.pseudo }}</v-card-title>
+
+                            <v-divider></v-divider>
+
+                            <v-list dense>
+                                <v-list-item>
+                                    <v-list-item-content>Prenom:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.prenom }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content>Nom:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.name }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content>Email:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.email }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content>Specialite:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.specialite }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content>Couleur:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.color }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content>Facture mois:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.facture_mois }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content>Facture coach:</v-list-item-content>
+                                    <v-list-item-content class="align-end">{{ item.prix_par_seance }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content><ModifCoach :nom="item.name" :prenom="item.prenom" :email="item.email" :specialite="item.specialite" :couleur="item.color" :facture_mois="item.facture_mois" :facture_coach="item.prix_par_seance" :submitmodif="submitmodif"></ModifCoach></v-list-item-content>
+                                    <v-list-item-content class="align-end"><SupprCoach :coach="item.prenom" :delete_coach="deletecoach"></SupprCoach></v-list-item-content>
+                                </v-list-item>
+                            </v-list>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </template>
+        </v-data-iterator>
+    </v-container>
+</template>
+
+<script>
+    import axios from "axios";
+    import ModifCoach from "../components/ModifCoach";
+    import SupprCoach from "../components/SupprCoach";
+    export default {
+        components: {ModifCoach, SupprCoach},
+        data: () => ({
+            itemsPerPage: 4,
+            coach: []
+        }),
+        async mounted() {
+            try {
+                const response = await axios.get("https://sportmanagementsystemapi.herokuapp.com/api/coach");
+                for (var x = 0; x < response.data.data.length; x++) {
+                    this.coach.push({
+                        id: response.data.data[x].id,
+                        pseudo: response.data.data[x].pseudo,
+                        name: response.data.data[x].nom,
+                        prenom : response.data.data[x].prenom,
+                        email: response.data.data[x].email,
+                        password: response.data.data[x].password,
+                        role: response.data.data[x].role,
+                        specialite: response.data.data[x].specialite,
+                        color: response.data.data[x].color,
+                        facture_mois : response.data.data[x].facture_mois,
+                        prix_par_seance : response.data.data[x].prix_par_seance,
+                    });
+                }
+            } catch (err) {
+            }
+        },
+        methods: {
+            submitmodif() {
+                var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + this.coach.id;
+                axios
+                    .put(url, {
+                        nom: this.nom,
+                        prenom: this.prenom,
+                        pseudo: this.pseudo,
+                        email: this.email,
+                        password: this.password,
+                        role: this.role,
+                        color: this.color,
+                    }, {
+                        headers: {
+                            token: localStorage.token
+                        }
+                    });
+                this.dialog = false;
+                window.location.reload();
+            },
+            delete_coach() {
+                var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + this.coach.id;
+                return url
+            }
+        }
+    }
+</script>
