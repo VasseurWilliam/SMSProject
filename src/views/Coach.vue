0f01bc3415dc91a089f544632eb237db3b4cfa56
william <vasseur.william1234@outlook.fr>
2020-01-30 11:35:26 +0100

    make it better

--- src/views/Coach.vue
+++ src/views/Coach.vue
@@ -51,7 +51,7 @@
                                     <v-list-item-content class="align-end">{{ item.prix_par_seance }}</v-list-item-content>
                                 </v-list-item>
                                 <v-list-item>
-                                    <v-list-item-content><ModifCoach :nom="item.name" :prenom="item.prenom" :email="item.email" :specialite="item.specialite" :couleur="item.color" :facture_mois="item.facture_mois" :facture_coach="item.prix_par_seance" submitmodif="{{ submitmodif(item.id) }}"></ModifCoach></v-list-item-content>
+                                    <v-list-item-content><ModifCoach :nom="item.name" :prenom="item.prenom" :email="item.email" :specialite="item.specialite" :couleur="item.color" :facture_mois="item.facture_mois" :facture_coach="item.prix_par_seance" :submitmodif="submitmodif"></ModifCoach></v-list-item-content>
                                     <v-list-item-content class="align-end"><SupprCoach :coach="item.prenom" :delete_coach="deletecoach"></SupprCoach></v-list-item-content>
                                 </v-list-item>
                             </v-list>
@@ -95,11 +95,11 @@
             }
         },
         methods: {
-            submitmodif: function (id) {
-                var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + id;
+            submitmodif: function (prenom) {
+                var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + 36;
                 axios
                     .put(url, {
-                        nom: "Broway"
+                        prenom: prenom
                     }, {
                         headers: {
                             token: localStorage.token
