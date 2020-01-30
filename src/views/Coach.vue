3bca375807a20ccabd7cbdb80df82ef30bc2820e
william <vasseur.william1234@outlook.fr>
2020-01-30 12:05:47 +0100

    make it better

--- src/views/Coach.vue
+++ src/views/Coach.vue
@@ -52,7 +52,7 @@
                                     <v-list-item-content class="align-end">{{ item.prix_par_seance }}</v-list-item-content>
                                 </v-list-item>
                                 <v-list-item>
-                                    <v-list-item-content><ModifCoach @click="(event) => { setIdCoach(event, item.id) }" :nom="item.name" :prenom="item.prenom" :email="item.email" :specialite="item.specialite" :couleur="item.color" :facture_mois="item.facture_mois" :facture_coach="item.prix_par_seance" :submitmodif="submitmodif"></ModifCoach></v-list-item-content>
+                                    <v-list-item-content><ModifCoach :nom="item.name" :prenom="item.prenom" :email="item.email" :specialite="item.specialite" :couleur="item.color" :facture_mois="item.facture_mois" :facture_coach="item.prix_par_seance" :submitmodif="submitmodif"></ModifCoach></v-list-item-content>
                                     <v-list-item-content class="align-end"><SupprCoach :coach="item.prenom" :delete_coach="deletecoach"></SupprCoach></v-list-item-content>
                                 </v-list-item>
                             </v-list>
@@ -98,7 +98,7 @@
         },
         methods: {
             submitmodif() {
-                var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + this.idCoach; //this.selectedElement.id;
+                var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + 36; //this.selectedElement.id;
                 axios
                     .put(url, {
                         nom: "Vasseure",
@@ -114,9 +114,6 @@
             delete_coach() {
                 var url = 'https://sportmanagementsystemapi.herokuapp.com/api/user/' + this.coach.id;
                 return url
-            },
-            setIdCoach: function (id) {
-                this.idCoach = id
             }
         }
     }
