324340c68f383ed9161d8c4cef37c33149948b5f
william <vasseur.william1234@outlook.fr>
2020-01-30 16:43:48 +0100

    make it better

--- src/views/Annonce.vue
+++ src/views/Annonce.vue
@@ -205,7 +205,8 @@ export default {
       try {
         await axios.delete(url, {
           headers: {
-            token: localStorage.token
+            token: localStorage.token,
+            id: localStorage.id
           }
         });
       } catch (err) {
