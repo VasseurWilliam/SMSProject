f2134415b52c121ca17d0460ea608096fce65e3a
william <vasseur.william1234@outlook.fr>
2020-01-31 10:35:38 +0100

    make it better

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import 'vuetify/dist/vuetify.css';
-import VueI18n from 'vue-i18n';
+import "vuetify/dist/vuetify.css";
+import VueI18n from "vue-i18n";
 
 Vue.use(Vuetify);
 Vue.use(VueI18n);
@@ -10,31 +10,31 @@ const messages = {
   en: {
     $vuetify: {
       dataIterator: {
-        rowsPerPageText: 'Items per page:',
-        pageText: '{0}-{1} of {2}',
-      },
-    },
+        rowsPerPageText: "Items per page:",
+        pageText: "{0}-{1} of {2}"
+      }
+    }
   },
   fr: {
     $vuetify: {
       dataIterator: {
-        rowsPerPageText: 'Objet par page:',
-        pageText: '{0}-{1} de {2}',
-      },
-    },
-  },
-}
+        rowsPerPageText: "Objet par page:",
+        pageText: "{0}-{1} de {2}"
+      }
+    }
+  }
+};
 
 const i18n = new VueI18n({
-  locale: 'fr',
-  messages,
-})
+  locale: "fr",
+  messages
+});
 
 export default new Vuetify({
   icons: {
     iconfont: "mdi"
   },
   lang: {
-    t: (key, ...params) => i18n.t(key, params),
-  },
+    t: (key, ...params) => i18n.t(key, params)
+  }
 });
